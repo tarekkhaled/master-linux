6d8709a47cc1052b688da32ef2c4c3df18697b26
tarek khaled <tarekt820@gmail.com>
2020-12-12 15:59:09 +0200

    remove port argument

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Logger = require("./logger");
 const logger = new Logger("Database Connection");
-const connect = async (mongodbURL, port) => {
+const connect = async (mongodbURL) => {
   try {
     await mongoose.connect(mongodbURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     });
-    logger.debug(`Connected successfully to mongoDB on port ${port}`);
+    logger.debug(`Connected successfully to mongoDB`);
   } catch (e) {
     logger.error(`Couldn't connected to mongodb due to ${e}`);
   }
