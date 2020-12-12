726a48844fdc79cd9003956f4b6ee3950ab04dc2
tarek khaled <tarekt820@gmail.com>
2020-12-12 20:21:08 +0200

    fix typo

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,4 @@
-export const getBonyan_authCookie = () => {
+export const getAuthCookie = () => {
   const cookiesObject = document.cookie.split("; ").reduce((prev, current) => {
     const [name, value] = current.split("=");
     prev[name] = value;
