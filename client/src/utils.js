d50a6a47f49ebea455a5b877e7d10dabc42c1a1b
tarek khaled <tarekt820@gmail.com>
2020-12-12 20:20:35 +0200

    add getAuthCookie, config functions

---
+++ client/src/utils.js
@@ -0,0 +1,15 @@
+export const getBonyan_authCookie = () => {
+  const cookiesObject = document.cookie.split("; ").reduce((prev, current) => {
+    const [name, value] = current.split("=");
+    prev[name] = value;
+    return prev;
+  }, {});
+  return cookiesObject?.albonyan_auth;
+};
+
+export const config = (userToken) => {
+  const configObject = {
+    headers: { Authorization: `Bearer ${userToken}` },
+  };
+  return configObject;
+};
